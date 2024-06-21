e06e6acafa01e059a7d1ee57f83a9d2329959fc7
thales <131139764+thalesmdp@users.noreply.github.com>
2024-06-20 23:10:25 -0300

    Add files via upload

---
+++ PBL.py
@@ -0,0 +1,7 @@
+import random
+
+def menu():
+    print('x=x=x=x=x=x=x=x=x=x=x=x=x=x=x\n|          DEEP SEA         |\nx=x=x=x=x=x=x=x=x=x=x=x=x=x=x')
+    print('Bem vindo ao jogo DEEP SEA!')
+
+menu()
